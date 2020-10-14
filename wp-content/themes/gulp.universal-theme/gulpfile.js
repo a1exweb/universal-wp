0dcd544fe7c0f8fd80a70f90c02d44c0c46f9711
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-10-14 16:41:45 +0300

    change gulp, add notify

---
+++ wp-content/themes/gulp.universal-theme/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+
+gulp.task('sass', function(done) {
+    gulp.src("../universal-theme/assets/sass/*.sass", "../universal-theme/assets/sass/*.scss")
+        .pipe(sass())
+        .on('error', notify.onError('Error: <%= error.message %>'))
+        .pipe(gulp.dest("../universal-theme/assets/css/"))
+        .pipe(browserSync.stream())
+    done();
+});
+
+gulp.task('serve', function(done) {
+
+    browserSync.init({
+        proxy: 'http://universal.local',
+        host: 'universal.local',
+        open: 'external'
+    });
+
+    gulp.watch("../universal-theme/assets/sass/*.sass", gulp.parallel('sass'));
+    gulp.watch("../universal-theme/assets/sass/*.scss", gulp.parallel('sass'));
+    gulp.watch("../universal-theme/*.*").on('change', () => {
+        browserSync.reload();
+        done();
+    });
+
+    done();
+});
+
+gulp.task('default', gulp.parallel('sass', 'serve'));
