c3fbf159cd918da926b74487d0e830ecb778e603
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-10-14 16:13:25 +0300

    add articles

--- wp-content/themes/universal-theme/front-page.php
+++ wp-content/themes/universal-theme/front-page.php
@@ -93,6 +93,7 @@
             $myposts = get_posts([ 
                 'numberposts' => 4,
                 'category_name' => 'articles',
+                'order' => 'ASC',
             ]);
 
             if( $myposts ){
@@ -115,6 +116,123 @@
             }
         wp_reset_postdata(); // Сбрасываем $post
         ?>
+    </ul>
+    <!-- ./article-list -->
+    <ul class="article-grid">
+    <?php		
+        global $post;
+
+        $query = new WP_Query( [
+            'posts_per_page' => 7,
+        ] );
+
+        if ( $query->have_posts() ) {
+            $cnt = 0;
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                $cnt++;
+                switch ($cnt) {
+                    case '1':
+                    ?>
+                        <li class="article-grid-item">
+                            <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+                                <span class="category-name"><?php 
+                                    $category = get_the_category(); 
+                                    echo $category[0]->name;
+                                ?></span>
+                                <h4 class="article-grid-title"><?php trim_title(50); ?></h4>
+                                <p class="article-grid-excerpt">
+                                    <?php echo get_the_excerpt(  ); ?>
+                                </p>
+                                <div class="article-grid-info">
+                                    <div class="author">
+                                        <?php $author_id = get_the_author_meta('ID'); ?>
+                                        <img src="<?php echo get_avatar_url($author_id); ?>" alt="Аватарка автора" class="author-avatar">
+                                        <span class="author-name"> <strong><?php the_author(); ?></strong>: <?php the_author_meta('description'); ?></span>
+                                    </div>
+                                    <div class="comments">
+                                        <img src="<?php echo get_template_directory_uri().'/assets/images/comment.svg'; ?>" alt="Иконка: комментарий" class="comments-icon">
+                                        <span class="comments-counter"><?php comments_number('0'); ?></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    <?php
+                    break;
+                
+                case '2': ?>
+                    <li class="article-grid-item">
+                        <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="article-grid-thumbnail">
+                            <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+                                <span class="tag">
+                                    <?php
+                                        $posttags = get_the_tags();
+                                        if ($posttags) {
+                                            echo $posttags[0]->name.'';
+                                        }
+                                    ?>
+                                </span>
+                                <span class="category-name"><?php 
+                                    $category = get_the_category(); 
+                                    echo $category[0]->name;
+                                ?></span>
+                                    <h4 class="article-grid-title"><?php trim_title(50); ?></h4>
+                                    <div class="article-grid-info">
+                                        <div class="author">
+                                            <?php $author_id = get_the_author_meta('ID'); ?>
+                                            <img src="<?php echo get_avatar_url($author_id); ?>" alt="Аватарка автора" class="author-avatar">
+                                            <div class="author-info">
+                                                <span class="author-name"> <strong><?php the_author(); ?></strong></span>
+                                                <span class="date"><?php the_time('j F'); ?></span>
+                                            <div class="comments">
+                                                <img src="<?php echo get_template_directory_uri().'/assets/images/comment-white.svg'; ?>" alt="Иконка: комментарий" class="comments-icon">
+                                                <span class="comments-counter"><?php comments_number('0'); ?></span>
+                                            </div>
+                                            <div class="likes">
+                                                <img src="<?php echo get_template_directory_uri().'/assets/images/heart.svg'; ?>" alt="Иконка: нравится">
+                                                <span class="likes-counter"><?php comments_number('0'); ?></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- ./article-grid-info -->
+                            </a>
+                        </li>
+                    <?php
+                    break;
+
+                    case '3': ?>
+                        <li class="article-grid-item">
+                            <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+                                <img src="<?php echo get_the_post_thumbnail_url( ); ?>" alt="">
+                                <h4 class="article-grid-title"><?php trim_title(50); ?></h4>
+                            </a>
+                        </li>
+                        <?php
+                        break;
+                    default: ?>
+                        <li class="article-grid-item article-grid-item-default">
+                            <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
+                                <h4 class="article-grid-title"><?php trim_title(50); ?></h4>
+                                <p class="article-grid-excerpt">
+                                    <?php echo get_the_excerpt(); ?>
+                                </p>
+                                <span class="date"><?php the_time('j F'); ?></span>
+                            </a>
+                        </li>
+                        <?php
+                        break;
+                }
+                ?>
+                <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+                <?php 
+            }
+        } else {
+            // Постов не найдено
+        }
+
+        wp_reset_postdata(); // Сбрасываем $post
+    ?>
     </ul>
 </div>
 <?php get_footer();
