4f4b7754c02690411d0f8f592f16e35ec57d7f66
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-10-28 13:14:06 +0300

    add footer

--- wp-content/themes/universal-theme/footer.php
+++ wp-content/themes/universal-theme/footer.php
@@ -1,3 +1,56 @@
+    <footer class="footer">
+        <div class="container">
+        <?php
+                if ( ! is_active_sidebar( 'sidebar-footer' ) ) {
+            return;
+        }
+        ?>
+
+        <div class="footer-menu-bar">
+            <?php dynamic_sidebar( 'sidebar-footer' ); ?>
+        </div>
+        <!-- /.footer-menu-bar -->
+        <div class="footer-info">
+            <?php
+                wp_nav_menu( [
+                    'theme_location' => 'footer_menu',
+                    'container'      =>  'nav',
+                    'menu_class'     =>  'footer-nav',
+                    'echo'           =>  true,
+                ] );
+            ?>
+            <?php
+                $instance = array(
+                    'facebook'  =>  'https://fb.com',
+                    'twitter'   =>  'https://twitter.com',
+                    'youtube'   =>  'https://youtube.com',
+                    'instagram' =>  'https://instagram.com',
+                    'title'     =>  '',
+                );
+                $args = array(
+                    'before_widget' => '<div class="footer-social">',
+                    'after_widget'  => '</div>',
+                );
+                the_widget( 'Social_Widget', $instance, $args);
+            ?>
+        </div>
+        <!-- /.footer-info -->
+        <?php
+                if ( ! is_active_sidebar( 'sidebar-footer-text' ) ) {
+            return;
+        }
+        ?>
+        <div class="footer-text-wrapper">
+            <?php dynamic_sidebar( 'sidebar-footer-text' ); ?>
+            <span class="footer-copyright">
+                <?php echo date('Y'). ' &copy; ' . get_bloginfo('name'); ?>
+            </span>
+            <!-- /.footer-copyright -->
+        </div>
+        <!-- /.footer-text-wrapper -->
+        </div>
+        <!-- /.container -->
+    </footer>
     <?php wp_footer(); ?>
 </body>
 </html>
